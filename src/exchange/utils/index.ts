a0b52386b0aba34167993c590db5dd3359325d27
ChefKai <74599990+ChefKai@users.noreply.github.com>
2021-04-22 14:49:38 +0200

    feat: V2 migration

--- src/exchange/utils/index.ts
+++ src/exchange/utils/index.ts
@@ -6,7 +6,7 @@ import { BEP20NameBytes } from "../../../generated/Factory/BEP20NameBytes";
 import { Factory as FactoryContract } from "../../../generated/templates/Pair/Factory";
 
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
-export const FACTORY_ADDRESS = "0xBCfCcbde45cE874adCB698cC183deBcF17952812";
+export const FACTORY_ADDRESS = "0x877fe7f4e22e21be397cd9364fafd4af4e15edb6";
 
 export let ZERO_BI = BigInt.fromI32(0);
 export let ONE_BI = BigInt.fromI32(1);
