a002a98487d7467bf283b2b9d3c9f7290d50cbd1
Chef Kai <74599990+ChefKai@users.noreply.github.com>
2021-05-11 22:05:09 +0200

    fix(exchange): Misc (#46)

--- src/exchange/factory.ts
+++ src/exchange/factory.ts
@@ -1,5 +1,4 @@
 /* eslint-disable prefer-const */
-import { log } from "@graphprotocol/graph-ts";
 import { PancakeFactory, Pair, Token, Bundle } from "../../generated/schema";
 import { Pair as PairTemplate } from "../../generated/templates";
 import { PairCreated } from "../../generated/Factory/Factory";
@@ -7,87 +6,76 @@ import {
   FACTORY_ADDRESS,
   ZERO_BD,
   ZERO_BI,
+  ONE_BI,
   fetchTokenSymbol,
   fetchTokenName,
   fetchTokenDecimals,
-  fetchTokenTotalSupply,
 } from "./utils";
 
 export function handlePairCreated(event: PairCreated): void {
   let factory = PancakeFactory.load(FACTORY_ADDRESS);
   if (factory === null) {
     factory = new PancakeFactory(FACTORY_ADDRESS);
-    factory.pairCount = 0;
+    factory.totalPairs = ZERO_BI;
+    factory.totalTransactions = ZERO_BI;
     factory.totalVolumeBNB = ZERO_BD;
     factory.totalLiquidityBNB = ZERO_BD;
     factory.totalVolumeUSD = ZERO_BD;
     factory.untrackedVolumeUSD = ZERO_BD;
     factory.totalLiquidityUSD = ZERO_BD;
-    factory.txCount = ZERO_BI;
 
-    // create new bundle
     let bundle = new Bundle("1");
     bundle.bnbPrice = ZERO_BD;
     bundle.save();
   }
-  factory.pairCount = factory.pairCount + 1;
+  factory.totalPairs = factory.totalPairs.plus(ONE_BI);
   factory.save();
 
-  // create the tokens
-  let token0 = Token.load(event.params.token0.toHexString());
-  let token1 = Token.load(event.params.token1.toHexString());
-
-  // fetch info if null
+  let token0 = Token.load(event.params.token0.toHex());
   if (token0 === null) {
-    token0 = new Token(event.params.token0.toHexString());
-    token0.symbol = fetchTokenSymbol(event.params.token0);
+    token0 = new Token(event.params.token0.toHex());
     token0.name = fetchTokenName(event.params.token0);
-    token0.totalSupply = fetchTokenTotalSupply(event.params.token0);
+    token0.symbol = fetchTokenSymbol(event.params.token0);
     let decimals = fetchTokenDecimals(event.params.token0);
-    // bail if we couldn't figure out the decimals
     if (decimals === null) {
-      log.debug("mybug the decimal on token 0 was null", []);
       return;
     }
-
     token0.decimals = decimals;
     token0.derivedBNB = ZERO_BD;
+    token0.derivedUSD = ZERO_BD;
     token0.tradeVolume = ZERO_BD;
     token0.tradeVolumeUSD = ZERO_BD;
     token0.untrackedVolumeUSD = ZERO_BD;
     token0.totalLiquidity = ZERO_BD;
-    // token0.allPairs = []
-    token0.txCount = ZERO_BI;
+    token0.totalTransactions = ZERO_BI;
+    token0.save();
   }
 
-  // fetch info if null
+  let token1 = Token.load(event.params.token1.toHex());
   if (token1 === null) {
-    token1 = new Token(event.params.token1.toHexString());
-    token1.symbol = fetchTokenSymbol(event.params.token1);
+    token1 = new Token(event.params.token1.toHex());
     token1.name = fetchTokenName(event.params.token1);
-    token1.totalSupply = fetchTokenTotalSupply(event.params.token1);
+    token1.symbol = fetchTokenSymbol(event.params.token1);
     let decimals = fetchTokenDecimals(event.params.token1);
-
-    // bail if we couldn't figure out the decimals
     if (decimals === null) {
       return;
     }
     token1.decimals = decimals;
     token1.derivedBNB = ZERO_BD;
+    token1.derivedUSD = ZERO_BD;
     token1.tradeVolume = ZERO_BD;
     token1.tradeVolumeUSD = ZERO_BD;
     token1.untrackedVolumeUSD = ZERO_BD;
     token1.totalLiquidity = ZERO_BD;
-    // token1.allPairs = []
-    token1.txCount = ZERO_BI;
+    token1.totalTransactions = ZERO_BI;
+    token1.save();
   }
 
-  let pair = new Pair(event.params.pair.toHexString()) as Pair;
+  let pair = new Pair(event.params.pair.toHex()) as Pair;
   pair.token0 = token0.id;
   pair.token1 = token1.id;
-  pair.createdAtTimestamp = event.block.timestamp;
-  pair.createdAtBlockNumber = event.block.number;
-  pair.txCount = ZERO_BI;
+  pair.name = token0.symbol.concat("-").concat(token1.symbol);
+  pair.totalTransactions = ZERO_BI;
   pair.reserve0 = ZERO_BD;
   pair.reserve1 = ZERO_BD;
   pair.trackedReserveBNB = ZERO_BD;
@@ -100,13 +88,9 @@ export function handlePairCreated(event: PairCreated): void {
   pair.untrackedVolumeUSD = ZERO_BD;
   pair.token0Price = ZERO_BD;
   pair.token1Price = ZERO_BD;
-
-  // create the tracked contract based on the template
-  PairTemplate.create(event.params.pair);
-
-  // save updated values
-  token0.save();
-  token1.save();
+  pair.block = event.block.number;
+  pair.timestamp = event.block.timestamp;
   pair.save();
-  factory.save();
+
+  PairTemplate.create(event.params.pair);
 }
