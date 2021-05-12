b8e5c4b548fe6695c262ffc4aad7e6de410cbc32
Chef Kai <74599990+ChefKai@users.noreply.github.com>
2021-05-12 13:35:45 +0200

    fix(smartchef): Blacklist addresses (#47)

--- src/smartchef/utils/index.ts
+++ src/smartchef/utils/index.ts
@@ -4,6 +4,10 @@ import { BigInt, BigDecimal } from "@graphprotocol/graph-ts";
 export let ZERO_BI = BigInt.fromI32(0);
 export let ONE_BI = BigInt.fromI32(1);
 
+export let BLACKLISTED_ADDRESSES: string[] = [
+  "0x2ba38552ee50c3450427c4afe72f8dd845205a33", // HachiMoon
+];
+
 export function exponentToBigDecimal(decimals: BigInt): BigDecimal {
   let bd = BigDecimal.fromString("1");
   for (let i = ZERO_BI; i.lt(decimals as BigInt); i = i.plus(ONE_BI)) {
