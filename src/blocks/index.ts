27714ec6b55799a4a9cfdc2f14bb434adae7bcf1
ChefKai <74599990+ChefKai@users.noreply.github.com>
2021-05-22 11:22:25 +0200

    refactor(blocks): Entity type + contract

--- src/blocks/index.ts
+++ src/blocks/index.ts
@@ -4,12 +4,12 @@ import { Block } from "../../generated/schema";
 
 export function handleBlock(block: ethereum.Block): void {
   let blockEntity = new Block(block.hash.toHex());
-  blockEntity.parentHash = block.parentHash.toHex();
-  blockEntity.unclesHash = block.unclesHash.toHex();
-  blockEntity.author = block.author.toHex();
-  blockEntity.stateRoot = block.stateRoot.toHex();
-  blockEntity.transactionsRoot = block.transactionsRoot.toHex();
-  blockEntity.receiptsRoot = block.receiptsRoot.toHex();
+  blockEntity.parentHash = block.parentHash;
+  blockEntity.unclesHash = block.unclesHash;
+  blockEntity.author = block.author;
+  blockEntity.stateRoot = block.stateRoot;
+  blockEntity.transactionsRoot = block.transactionsRoot;
+  blockEntity.receiptsRoot = block.receiptsRoot;
   blockEntity.number = block.number;
   blockEntity.gasUsed = block.gasUsed;
   blockEntity.gasLimit = block.gasLimit;
