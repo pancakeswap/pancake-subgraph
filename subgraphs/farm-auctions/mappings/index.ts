8b4c54ce99e746e11364a06b9d943dc4ea7be392
ChefKai <74599990+ChefKai@users.noreply.github.com>
2021-08-25 13:38:14 +0200

    fix: ABI's binding

--- subgraphs/farm-auctions/mappings/index.ts
+++ subgraphs/farm-auctions/mappings/index.ts
@@ -1,11 +1,19 @@
 /* eslint-disable prefer-const */
-import { BigInt } from "@graphprotocol/graph-ts";
-import { Bidder } from "../generated/schema";
-import { AuctionBid, WhitelistAdd, WhitelistRemove } from "../generated/FarmAuction/FarmAuction";
+import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
+import { Auction, Bidder } from "../generated/schema";
+import {
+  AuctionBid,
+  AuctionClose,
+  AuctionStart,
+  WhitelistAdd,
+  WhitelistRemove,
+} from "../generated/FarmAuction/FarmAuction";
+import { toBigDecimal } from "./utils";
 
 // BigNumber-like references
 let ZERO_BI = BigInt.fromI32(0);
 let ONE_BI = BigInt.fromI32(1);
+let ZERO_BD = BigDecimal.fromString("0");
 
 /**
  * Whitelisting service
@@ -17,6 +25,7 @@ export function handleWhitelistAdd(event: WhitelistAdd): void {
     bidder = new Bidder(event.params.account.toHex());
     bidder.isWhitelisted = true;
     bidder.totalBids = ZERO_BI;
+    bidder.totalCake = ZERO_BD;
     bidder.block = event.block.number;
     bidder.timestamp = event.block.timestamp;
     bidder.save();
@@ -31,6 +40,7 @@ export function handleWhitelistRemove(event: WhitelistRemove): void {
     bidder = new Bidder(event.params.account.toHex());
     bidder.isWhitelisted = false;
     bidder.totalBids = ZERO_BI;
+    bidder.totalCake = ZERO_BD;
     bidder.block = event.block.number;
     bidder.timestamp = event.block.timestamp;
     bidder.save();
@@ -43,10 +53,40 @@ export function handleWhitelistRemove(event: WhitelistRemove): void {
  * Auction service
  */
 
+export function handleAuctionStart(event: AuctionStart): void {
+  let auction = new Auction(event.params.auctionId.toString());
+  auction.totalBids = ZERO_BI;
+  auction.totalCake = ZERO_BD;
+  auction.status = "Open";
+  auction.startBlock = event.params.startBlock;
+  auction.endBlock = event.params.endBlock;
+  auction.initialBidAmount = toBigDecimal(event.params.initialBidAmount);
+  auction.leaderboard = event.params.leaderboard;
+  auction.leaderboardThreshold = ZERO_BD;
+  auction.save();
+}
+
+export function handleAuctionClose(event: AuctionClose): void {
+  let auction = Auction.load(event.params.auctionId.toString());
+  if (auction !== null) {
+    auction.status = "Close";
+    auction.leaderboardThreshold = toBigDecimal(event.params.participationLimit);
+    auction.save();
+  }
+}
+
 export function handleAuctionBid(event: AuctionBid): void {
+  let auction = Auction.load(event.params.auctionId.toString());
+  if (auction !== null) {
+    auction.totalBids = auction.totalBids.plus(ONE_BI);
+    auction.totalCake = auction.totalCake.plus(toBigDecimal(event.params.amount));
+    auction.save();
+  }
+
   let bidder = Bidder.load(event.params.account.toHex());
   if (bidder !== null) {
     bidder.totalBids = bidder.totalBids.plus(ONE_BI);
+    bidder.totalCake = bidder.totalCake.plus(toBigDecimal(event.params.amount));
     bidder.save();
   }
 }
