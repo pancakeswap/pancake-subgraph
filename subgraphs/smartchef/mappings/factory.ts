1c6526fd134882f3eba41a955bfffa94cc011a01
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-03-15 23:22:28 +0800

    chore: Remove no used

--- subgraphs/smartchef/mappings/factory.ts
+++ subgraphs/smartchef/mappings/factory.ts
@@ -39,11 +39,6 @@ export function handleNewSmartChefContract(event: NewSmartChefContract): void {
 }
 
 export function handleNewSmartChefContractV2(event: NewSmartChefContract): void {
-  // Do not process some SmartChef smart contract, hiccup.
-  if (BLACKLISTED_ADDRESSES.includes(event.params.smartChef.toHex())) {
-    return;
-  }
-
   let factory = Factory.load(FACTORY_V2_ADDRESS);
   if (factory === null) {
     factory = new Factory(FACTORY_V2_ADDRESS);
