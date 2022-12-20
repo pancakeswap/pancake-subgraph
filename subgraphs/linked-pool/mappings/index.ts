2697b8e0d587add280b0c32fb1b0948e9fe04754
Chef Huan <huan@pancakeswap.com>
2022-12-20 11:54:43 +0100

    feat(linked-pool): Calculate price include decimals

--- subgraphs/linked-pool/mappings/index.ts
+++ subgraphs/linked-pool/mappings/index.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prefer-const */
 import { Swap } from "../generated/schema";
 import { Swap as SwapEven } from "../generated/PancakeSwapMMPool/PancakeSwapMMPool";
+import { convertTokenToDecimal, getOrCreateToken } from "../utils";
 
 export function handleSwap(event: SwapEven): void {
   let id = event.transaction.hash
@@ -14,6 +15,9 @@ export function handleSwap(event: SwapEven): void {
 
   let swap = Swap.load(id);
   if (swap === null) {
+    let baseToken = getOrCreateToken(event.params.baseToken);
+    let quoteToken = getOrCreateToken(event.params.quoteToken);
+
     swap = new Swap(id);
     swap.user = event.params.user.toHex();
     swap.mm = event.params.mm.toHex();
@@ -21,8 +25,8 @@ export function handleSwap(event: SwapEven): void {
     swap.mmTreasury = event.params.mmTreasury.toHex();
     swap.baseToken = event.params.baseToken.toHex();
     swap.quoteToken = event.params.quoteToken.toHex();
-    swap.baseTokenAmount = event.params.baseTokenAmount;
-    swap.quoteTokenAmount = event.params.quoteTokenAmount;
+    swap.baseTokenAmount = convertTokenToDecimal(event.params.baseTokenAmount, baseToken.decimals);
+    swap.quoteTokenAmount = convertTokenToDecimal(event.params.quoteTokenAmount, quoteToken.decimals);
     swap.timestamp = event.block.timestamp;
     swap.save();
   }
