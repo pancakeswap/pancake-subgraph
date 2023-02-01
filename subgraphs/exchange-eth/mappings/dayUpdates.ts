28b55165c8889539c91a7284f64b441a8c31f760
chef-huan <93581122+chef-huan@users.noreply.github.com>
2023-02-01 15:53:03 +0100

    chore(exchange-eth): Add subgraph for eth network (#180)
    * chore(exchange-eth): Add subgraph for eth network
    
    * fix(exchange-eth): Addresses
    
    * chore(exchange-eth): Add token0, token1 to events
    
    * chore(exchange-eth): Update stable pairs addresses

---
+++ subgraphs/exchange-eth/mappings/dayUpdates.ts
@@ -0,0 +1,120 @@
+/* eslint-disable prefer-const */
+import {
+  Bundle,
+  Pair,
+  PairDayData,
+  PairHourData,
+  PancakeDayData,
+  PancakeFactory,
+  Token,
+  TokenDayData,
+} from "../generated/schema";
+import { BigDecimal, BigInt, ethereum } from "@graphprotocol/graph-ts";
+import { FACTORY_ADDRESS, ONE_BI, ZERO_BD, ZERO_BI } from "./utils";
+
+export function updatePancakeDayData(event: ethereum.Event): PancakeDayData {
+  let pancake = PancakeFactory.load(FACTORY_ADDRESS);
+  let timestamp = event.block.timestamp.toI32();
+  let dayID = timestamp / 86400;
+  let dayStartTimestamp = dayID * 86400;
+
+  let pancakeDayData = PancakeDayData.load(dayID.toString());
+  if (pancakeDayData === null) {
+    pancakeDayData = new PancakeDayData(dayID.toString());
+    pancakeDayData.date = dayStartTimestamp;
+    pancakeDayData.dailyVolumeUSD = ZERO_BD;
+    pancakeDayData.dailyVolumeETH = ZERO_BD;
+    pancakeDayData.totalVolumeUSD = ZERO_BD;
+    pancakeDayData.totalVolumeETH = ZERO_BD;
+    pancakeDayData.dailyVolumeUntracked = ZERO_BD;
+  }
+  pancakeDayData.totalLiquidityUSD = pancake.totalLiquidityUSD;
+  pancakeDayData.totalLiquidityETH = pancake.totalLiquidityETH;
+  pancakeDayData.totalTransactions = pancake.totalTransactions;
+  pancakeDayData.save();
+
+  return pancakeDayData as PancakeDayData;
+}
+
+export function updatePairDayData(event: ethereum.Event): PairDayData {
+  let timestamp = event.block.timestamp.toI32();
+  let dayID = timestamp / 86400;
+  let dayStartTimestamp = dayID * 86400;
+  let dayPairID = event.address.toHex().concat("-").concat(BigInt.fromI32(dayID).toString());
+  let pair = Pair.load(event.address.toHex());
+  let pairDayData = PairDayData.load(dayPairID);
+  if (pairDayData === null) {
+    pairDayData = new PairDayData(dayPairID);
+    pairDayData.date = dayStartTimestamp;
+    pairDayData.token0 = pair.token0;
+    pairDayData.token1 = pair.token1;
+    pairDayData.pairAddress = event.address;
+    pairDayData.dailyVolumeToken0 = ZERO_BD;
+    pairDayData.dailyVolumeToken1 = ZERO_BD;
+    pairDayData.dailyVolumeUSD = ZERO_BD;
+    pairDayData.dailyTxns = ZERO_BI;
+  }
+  pairDayData.totalSupply = pair.totalSupply;
+  pairDayData.reserve0 = pair.reserve0;
+  pairDayData.reserve1 = pair.reserve1;
+  pairDayData.reserveUSD = pair.reserveUSD;
+  pairDayData.dailyTxns = pairDayData.dailyTxns.plus(ONE_BI);
+  pairDayData.save();
+
+  return pairDayData as PairDayData;
+}
+
+export function updatePairHourData(event: ethereum.Event): PairHourData {
+  let timestamp = event.block.timestamp.toI32();
+  let hourIndex = timestamp / 3600;
+  let hourStartUnix = hourIndex * 3600;
+  let hourPairID = event.address.toHex().concat("-").concat(BigInt.fromI32(hourIndex).toString());
+  let pair = Pair.load(event.address.toHex());
+  let pairHourData = PairHourData.load(hourPairID);
+  if (pairHourData === null) {
+    pairHourData = new PairHourData(hourPairID);
+    pairHourData.hourStartUnix = hourStartUnix;
+    pairHourData.pair = event.address.toHex();
+    pairHourData.hourlyVolumeToken0 = ZERO_BD;
+    pairHourData.hourlyVolumeToken1 = ZERO_BD;
+    pairHourData.hourlyVolumeUSD = ZERO_BD;
+    pairHourData.hourlyTxns = ZERO_BI;
+  }
+  pairHourData.totalSupply = pair.totalSupply;
+  pairHourData.reserve0 = pair.reserve0;
+  pairHourData.reserve1 = pair.reserve1;
+  pairHourData.reserveUSD = pair.reserveUSD;
+  pairHourData.hourlyTxns = pairHourData.hourlyTxns.plus(ONE_BI);
+  pairHourData.save();
+
+  return pairHourData as PairHourData;
+}
+
+export function updateTokenDayData(token: Token, event: ethereum.Event): TokenDayData {
+  let bundle = Bundle.load("1");
+  let timestamp = event.block.timestamp.toI32();
+  let dayID = timestamp / 86400;
+  let dayStartTimestamp = dayID * 86400;
+  let tokenDayID = token.id.toString().concat("-").concat(BigInt.fromI32(dayID).toString());
+
+  let tokenDayData = TokenDayData.load(tokenDayID);
+  if (tokenDayData === null) {
+    tokenDayData = new TokenDayData(tokenDayID);
+    tokenDayData.date = dayStartTimestamp;
+    tokenDayData.token = token.id;
+    tokenDayData.priceUSD = token.derivedETH.times(bundle.ethPrice);
+    tokenDayData.dailyVolumeToken = ZERO_BD;
+    tokenDayData.dailyVolumeETH = ZERO_BD;
+    tokenDayData.dailyVolumeUSD = ZERO_BD;
+    tokenDayData.dailyTxns = ZERO_BI;
+    tokenDayData.totalLiquidityUSD = ZERO_BD;
+  }
+  tokenDayData.priceUSD = token.derivedETH.times(bundle.ethPrice);
+  tokenDayData.totalLiquidityToken = token.totalLiquidity;
+  tokenDayData.totalLiquidityETH = token.totalLiquidity.times(token.derivedETH as BigDecimal);
+  tokenDayData.totalLiquidityUSD = tokenDayData.totalLiquidityETH.times(bundle.ethPrice);
+  tokenDayData.dailyTxns = tokenDayData.dailyTxns.plus(ONE_BI);
+  tokenDayData.save();
+
+  return tokenDayData as TokenDayData;
+}
