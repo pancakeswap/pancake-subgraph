e3576069fddb4cc0a8d36764ca30d429cd1223ff
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-03-25 13:56:13 +0800

    feat: Add protocl collect

--- subgraphs/exchange-v3/template/mappings/core.ts
+++ subgraphs/exchange-v3/template/mappings/core.ts
@@ -9,15 +9,15 @@ import {
   Mint as MintEvent,
   Swap as SwapEvent,
   Collect as CollectEvent,
+  CollectProtocol as CollectProtocolEvent,
 } from "../generated/templates/Pool/Pool";
-import { convertTokenToDecimal, loadTransaction, safeDiv } from "../utils";
+import { convertTokenToDecimal, loadTransaction } from "../utils";
 import { FACTORY_ADDRESS, ONE_BI, TWO_BD, ZERO_BD, ZERO_BI } from "../utils/constants";
 import {
   AmountType,
   findEthPerToken,
   getAdjustedAmounts,
   getEthPriceInUSD,
-  getTrackedAmountUSD,
   sqrtPriceX96ToTokenPrices,
 } from "../utils/pricing";
 import {
@@ -569,4 +569,36 @@ export function handleCollect(event: CollectEvent): void {
   factory.save();
   pool.save();
   collect.save();
+}
+
+export function handleCollectProtocol(event: CollectProtocolEvent): void {
+  // update fee growth
+  let pool = Pool.load(event.address.toHexString());
+  let factory = Factory.load(FACTORY_ADDRESS);
+  let token0 = Token.load(pool.token0);
+  let token1 = Token.load(pool.token1);
+
+  // Get formatted amounts collected.
+  let amount0 = convertTokenToDecimal(event.params.amount0, token0.decimals);
+  let amount1 = convertTokenToDecimal(event.params.amount1, token1.decimals);
+
+  // Adjust pool TVL based on amount collected.
+  let oldPoolTVLETH = pool.totalValueLockedETH;
+  let oldPoolTVLETHUntracked = pool.totalValueLockedETHUntracked;
+  pool.totalValueLockedToken0 = pool.totalValueLockedToken0.minus(amount0);
+  pool.totalValueLockedToken1 = pool.totalValueLockedToken1.minus(amount1);
+  token0.totalValueLocked = token0.totalValueLocked.minus(amount0);
+  token1.totalValueLocked = token1.totalValueLocked.minus(amount1);
+  updateDerivedTVLAmounts(pool as Pool, factory as Factory, oldPoolTVLETH, oldPoolTVLETHUntracked);
+
+  // Update transaction counts.
+  factory.txCount = factory.txCount.plus(ONE_BI);
+  token0.txCount = token0.txCount.plus(ONE_BI);
+  token1.txCount = token1.txCount.plus(ONE_BI);
+  pool.txCount = pool.txCount.plus(ONE_BI);
+
+  token0.save();
+  token1.save();
+  factory.save();
+  pool.save();
 }
