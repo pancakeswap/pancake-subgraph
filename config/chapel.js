57d2ed94258ef76670272bb3e42b61e8d710938b
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-03-27 17:58:15 +0800

    chore: Add whitelist

--- config/chapel.js
+++ config/chapel.js
@@ -17,6 +17,8 @@ module.exports = {
       "0xae13d989dac2f0debff460ac112a837c89baa7cd",
       "0x828e3fc56dd48e072e3b6f3c4fd4ddb4733c2c5e",
       "0xc1ed9955c11585f47d0d6bfbc29034349a746a81",
+      "0x0fb5d7c73fa349a90392f873a4fa1ecf6a3d0a96",
+      "0xab1a4d4f1d656d2450692d237fdd6c7f9146e814",
     ],
     nonfungiblePositionManagerAddress: "0xee02dc74894c7e973e058a676d513f25f4e1bec7",
     nonfungiblePositionManagerStartBlock: 28284500,
