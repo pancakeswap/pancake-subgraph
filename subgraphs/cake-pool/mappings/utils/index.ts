3d4828f16ebe1f5d6772ad6a85d2710fbfc5ad6f
Chef Huan <huan@pancakeswap.com>
2023-04-11 16:36:17 +0700

    fix: On-chain amount calculation. Remove withdraw

--- subgraphs/cake-pool/mappings/utils/index.ts
+++ subgraphs/cake-pool/mappings/utils/index.ts
@@ -1,8 +1,11 @@
 /* eslint-disable prefer-const */
 import { User } from "../../generated/schema";
-import { BigInt } from "@graphprotocol/graph-ts";
+import { Address, BigInt, log } from "@graphprotocol/graph-ts";
+import { CakePool } from "../../generated/CakePool/CakePool";
 
 export let ZERO_BI = BigInt.fromI32(0);
+export let BI_1E18 = BigInt.fromString("1000000000000000000");
+export let CAKE_POOL_CONTRACT = CakePool.bind(Address.fromString("0x45c54210128a065de780C4B0Df3d16664f7f859e"));
 
 export function getOrCreateUser(id: string): User {
   let user = User.load(id);
@@ -15,4 +18,25 @@ export function getOrCreateUser(id: string): User {
     user.save();
   }
   return user as User;
+}
+
+export function getUserLockAmount(userAddress: string): BigInt {
+  let userInfoResult = CAKE_POOL_CONTRACT.try_userInfo(Address.fromString(userAddress));
+  let cakeAmount = ZERO_BI;
+  if (userInfoResult.reverted) {
+    log.warning("Unable to fetch try_userInfo for {}", [CAKE_POOL_CONTRACT._address.toHex()]);
+  } else {
+    let userInfo = userInfoResult.value;
+    let shares = userInfo.value0;
+    let userBoostedShare = userInfo.value6;
+    let pricePerFullShareResult = CAKE_POOL_CONTRACT.try_getPricePerFullShare();
+    if (pricePerFullShareResult.reverted) {
+      log.warning("Unable to fetch try_getPricePerFullShare for {}", [CAKE_POOL_CONTRACT._address.toHex()]);
+    } else {
+      let pricePerFullShare = pricePerFullShareResult.value;
+      // cake amount (wei), in flexible staking, userInfo.userBoostedShare should be 0.
+      cakeAmount = shares.times(pricePerFullShare).div(BI_1E18).minus(userBoostedShare);
+    }
+  }
+  return cakeAmount;
 }
