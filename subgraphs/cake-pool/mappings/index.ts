654389ef151dfc0ae93f1b23cf998b087530faa4
chef-huan <93581122+chef-huan@users.noreply.github.com>
2023-04-14 15:03:23 +0700

    Fix cake pool (#211)
    * fix: Indexing start block (#206)
    
    * fix: On-chain amount calculation. Remove withdraw
    
    * fix: Copy from indexed graft

--- subgraphs/cake-pool/mappings/index.ts
+++ subgraphs/cake-pool/mappings/index.ts
@@ -1,8 +1,8 @@
 /* eslint-disable prefer-const */
 import { BigInt, log } from "@graphprotocol/graph-ts";
 import { CakePool } from "../generated/schema";
-import { Deposit, Lock, NewMaxLockDuration, Unlock } from "../generated/CakePool/CakePool";
-import { getOrCreateUser, ZERO_BI } from "./utils";
+import { Lock, NewMaxLockDuration, Unlock } from "../generated/CakePool/CakePool";
+import { getOrCreateUser, getUserLockAmount, ZERO_BI } from "./utils";
 
 export function startCountdown(event: NewMaxLockDuration): void {
   let cakePool = CakePool.load("1");
@@ -14,36 +14,6 @@ export function startCountdown(event: NewMaxLockDuration): void {
     cakePool.blockNumber = event.block.number;
     cakePool.totalLocked = ZERO_BI;
     cakePool.maxLockDuration = event.params.maxLockDuration;
-    cakePool.usersWithLockedCake = [];
-    cakePool.save();
-  }
-}
-
-export function handleDeposit(event: Deposit): void {
-  let cakePool = CakePool.load("1");
-  if (cakePool !== null) {
-    let user = getOrCreateUser(event.params.sender.toHex());
-    // log.info("Deposit. Duration - {}. Lock amount - {}", [
-    //   event.params.duration.toString(),
-    //   event.params.amount.toString(),
-    // ]);
-
-    if (event.params.duration.gt(ZERO_BI)) {
-      if (user.lockEndTime < event.block.timestamp) {
-        user.lockStartTime = event.block.timestamp;
-        user.lockEndTime = event.block.timestamp.plus(event.params.duration);
-      } else {
-        user.lockEndTime = user.lockEndTime.plus(event.params.duration);
-      }
-      cakePool.usersWithLockedCake = cakePool.usersWithLockedCake.concat([event.params.sender.toHex()]);
-    } else {
-      if (user.totalLocked.gt(ZERO_BI)) {
-        user.totalLocked = user.totalLocked.plus(event.params.amount);
-        cakePool.totalLocked = cakePool.totalLocked.plus(event.params.amount);
-      }
-    }
-
-    user.save();
     cakePool.save();
   }
 }
@@ -52,15 +22,22 @@ export function handleLock(event: Lock): void {
   let cakePool = CakePool.load("1");
   if (cakePool !== null) {
     let user = getOrCreateUser(event.params.sender.toHex());
-    // log.info("Lock. Duration - {}. Lock amount - {}", [
-    //   event.params.lockedDuration.toString(),
-    //   event.params.lockedAmount.toString(),
-    // ]);
 
-    cakePool.totalLocked = cakePool.totalLocked.minus(user.totalLocked).plus(event.params.lockedAmount);
+    if (event.params.lockedDuration.gt(ZERO_BI)) {
+      if (user.lockEndTime < event.block.timestamp) {
+        user.lockStartTime = event.block.timestamp;
+        user.lockEndTime = event.block.timestamp.plus(event.params.lockedDuration);
+      } else {
+        user.lockEndTime = user.lockEndTime.plus(event.params.lockedDuration);
+      }
+    }
+
+    let userTotalLocked = getUserLockAmount(user.id);
+
+    cakePool.totalLocked = cakePool.totalLocked.plus(userTotalLocked);
     cakePool.save();
 
-    user.totalLocked = event.params.lockedAmount;
+    user.totalLocked = userTotalLocked;
     user.duration = event.params.lockedDuration;
     user.locked = true;
     user.save();
@@ -71,12 +48,11 @@ export function handleUnlock(event: Unlock): void {
   let cakePool = CakePool.load("1");
   if (cakePool !== null) {
     let user = getOrCreateUser(event.params.sender.toHex());
-    // log.info("Unlock. Amount - {}", [event.params.amount.toString()]);
 
-    cakePool.totalLocked = cakePool.totalLocked.minus(event.params.amount);
+    cakePool.totalLocked = cakePool.totalLocked.minus(user.totalLocked);
     cakePool.save();
 
-    user.totalLocked = user.totalLocked.minus(event.params.amount);
+    user.totalLocked = ZERO_BI;
     user.lockStartTime = ZERO_BI;
     user.lockEndTime = ZERO_BI;
     user.locked = false;
