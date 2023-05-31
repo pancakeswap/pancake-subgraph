30d2266a2b764bd0f165ce14171c8851279172e9
Chef Huan <huan@pancakeswap.com>
2023-05-31 12:12:28 +0700

    chore: Add user position calculation

--- subgraphs/user-position-v3/template/mappings/position-manager.ts
+++ subgraphs/user-position-v3/template/mappings/position-manager.ts
@@ -1,19 +1,34 @@
 /* eslint-disable prefer-const */
+import { Address } from "@graphprotocol/graph-ts";
 import {
   DecreaseLiquidity,
   IncreaseLiquidity,
   Transfer,
 } from "../generated/NonfungiblePositionManager/NonfungiblePositionManager";
-import { loadTransaction } from "../utils";
+import { loadTransaction } from "../utils/schema";
+import { ADDRESS_ZERO } from "../utils/constants";
+import { UserPosition } from "../generated/schema";
 
 export function handleIncreaseLiquidity(event: IncreaseLiquidity): void {
   let transaction = loadTransaction(event);
+  transaction.tokenId = event.params.tokenId;
+  transaction.save();
 }
 
 export function handleDecreaseLiquidity(event: DecreaseLiquidity): void {
   let transaction = loadTransaction(event);
+  transaction.tokenId = event.params.tokenId;
+  transaction.save();
 }
 
 export function handleTransfer(event: Transfer): void {
   let transaction = loadTransaction(event);
+  if (event.params.from.equals(Address.fromString(ADDRESS_ZERO))) {
+    transaction.positionOwner = event.params.to;
+  } else {
+    let userPosition = UserPosition.load(event.params.tokenId.toString());
+    userPosition.owner = event.params.to;
+    userPosition.save();
+  }
+  transaction.save();
 }
--- subgraphs/user-position-v3/template/utils/index.ts
+++ subgraphs/user-position-v3/template/utils/index.ts
@@ -1,7 +1,6 @@
 /* eslint-disable prefer-const */
-import { BigInt, BigDecimal, ethereum } from "@graphprotocol/graph-ts";
-import { Transaction } from "../generated/schema";
-import { ONE_BI, ZERO_BI, ZERO_BD, ONE_BD } from "./constants";
+import { BigInt, BigDecimal } from "@graphprotocol/graph-ts";
+import { ONE_BI, ZERO_BI } from "./constants";
 
 export function exponentToBigDecimal(decimals: BigInt): BigDecimal {
   let bd = BigDecimal.fromString("1");
@@ -9,31 +8,6 @@ export function exponentToBigDecimal(decimals: BigInt): BigDecimal {
     bd = bd.times(BigDecimal.fromString("10"));
   }
   return bd;
-}
-
-// return 0 if denominator is 0 in division
-export function safeDiv(amount0: BigDecimal, amount1: BigDecimal): BigDecimal {
-  if (amount1.equals(ZERO_BD)) {
-    return ZERO_BD;
-  } else {
-    return amount0.div(amount1);
-  }
-}
-
-export function tokenAmountToDecimal(tokenAmount: BigInt, exchangeDecimals: BigInt): BigDecimal {
-  if (exchangeDecimals == ZERO_BI) {
-    return tokenAmount.toBigDecimal();
-  }
-  return tokenAmount.toBigDecimal().div(exponentToBigDecimal(exchangeDecimals));
-}
-
-export function equalToZero(value: BigDecimal): boolean {
-  const formattedVal = parseFloat(value.toString());
-  const zero = parseFloat(ZERO_BD.toString());
-  if (zero == formattedVal) {
-    return true;
-  }
-  return false;
 }
 
 export function isNullEthValue(value: string): boolean {
@@ -45,17 +19,4 @@ export function convertTokenToDecimal(tokenAmount: BigInt, exchangeDecimals: Big
     return tokenAmount.toBigDecimal();
   }
   return tokenAmount.toBigDecimal().div(exponentToBigDecimal(exchangeDecimals));
-}
-
-export function loadTransaction(event: ethereum.Event): Transaction {
-  let transaction = Transaction.load(event.transaction.hash.toHexString());
-  if (transaction === null) {
-    transaction = new Transaction(event.transaction.hash.toHexString());
-  }
-  transaction.blockNumber = event.block.number;
-  transaction.timestamp = event.block.timestamp;
-  transaction.gasUsed = event.transaction.gasUsed;
-  transaction.gasPrice = event.transaction.gasPrice;
-  transaction.save();
-  return transaction as Transaction;
 }
