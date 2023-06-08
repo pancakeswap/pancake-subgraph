02c18e672602c0b25a1fbf6026c64f6a77168954
rabbid <rabbid@pancakeswap.com>
2023-06-08 16:48:14 +0800

    build: Update affiliate subgraph address and block

--- subgraphs/affiliate-program/src/mapping.ts
+++ subgraphs/affiliate-program/src/mapping.ts
@@ -1,12 +1,14 @@
-import { BigInt } from "@graphprotocol/graph-ts";
 import { Claim } from "../generated/AffiliateProgram/AffiliateProgram";
 import { ClaimRecord } from "../generated/schema";
 
 export function handleClaim(event: Claim): void {
-  let entity = new ClaimRecord(`${event.params.claimer.toHex()}-${event.params.nonce}-${event.params.claimType}`);
+  let claimType = event.params.claimType == 0 ? "AFFILIATE" : "USER";
+  let entity = new ClaimRecord(
+    event.params.claimer.toHexString().concat("-").concat(claimType).concat("-").concat(event.params.nonce.toString())
+  );
   entity.nonce = event.params.nonce;
   entity.claimer = event.params.claimer;
-  entity.type = event.params.claimType == 0 ? "AFFILIATE" : "USER";
+  entity.type = claimType;
   entity.amount = event.params.amount;
   entity.hash = event.transaction.hash;
   entity.claimTime = event.block.timestamp;
