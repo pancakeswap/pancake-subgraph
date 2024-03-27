21610649a39390bd033641b6e8d88baab26cbc2a
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2024-03-27 10:38:21 +0800

    chore: Add zksync v3 whitelist (#258)

--- config/zksync-era.js
+++ config/zksync-era.js
@@ -16,6 +16,8 @@ module.exports = {
       "0x493257fd37edb34451f62edf8d2a0c418852ba4c", // USDT
       "0x3355df6d4c9c3035724fd0e3914de96a5a83aaf4", // USDC
       "0x2039bb4116b4efc145ec4f0e2ea75012d6c0f181", // BUSD
+      "0x4b9eb6c0b6ea15176bbf62841c6b2a8a398cb656", // DAI
+      "0x8e86e46278518efc1c5ced245cba2c7e3ef11557", // USD+
     ],
     whitelistAddresses: [
       WETH,
@@ -23,6 +25,11 @@ module.exports = {
       "0x2039bb4116b4efc145ec4f0e2ea75012d6c0f181", // BUSD
       "0x3355df6d4c9c3035724fd0e3914de96a5a83aaf4", // USDC
       "0xbbeb516fb02a01611cbbe0453fe3c580d7281011", // WBTC
+      "0x32fd44bb869620c0ef993754c8a00be67c464806", // rETH
+      "0x703b52f2b28febcb60e1372858af5b18849fe867", // wstETH
+      "0x3a287a06c66f9e95a56327185ca2bdf5f031cecd", // CAKE
+      "0x4b9eb6c0b6ea15176bbf62841c6b2a8a398cb656", // DAI
+      "0x8e86e46278518efc1c5ced245cba2c7e3ef11557", // USD+
     ],
     nonfungiblePositionManagerAddress: "0xa815e2ed7f7d5b0c49fda367f249232a1b9d2883",
     nonfungiblePositionManagerStartBlock: 8640657,
